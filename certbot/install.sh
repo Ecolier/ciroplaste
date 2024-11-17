6b6a54060fa9140dfa98f25408e8b19f52b54582
Evan Gruère <evan.gruere@gmail.com>
2024-11-17 18:04:05 +0100

    Fixed remove tail

--- certbot/install.sh
+++ certbot/install.sh
@@ -3,7 +3,5 @@
 secret=`cat $DNS_CLOUDFLARE_API_TOKEN_FILE`
 echo "dns_cloudflare_api_token = $secret" > cloudflare.ini
 
-tail -f /dev/null
-
 certbot certonly --dns-cloudflare-credentials /app/cloudflare.ini -d *.ciroplaste.com
 
