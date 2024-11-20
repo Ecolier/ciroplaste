b09a478de2ec0dfef1eb3941000e3cc69004e630
Evan Gruère <evan.gruere@gmail.com>
2024-11-20 23:44:58 +0100

    Fixed linting error

--- apps/web/src/app/[locale]/explore/sitemap.ts
+++ apps/web/src/app/[locale]/explore/sitemap.ts
@@ -4,11 +4,7 @@ import { getLocale } from "next-intl/server";
 
 const contentBaseUrl = process.env.NEXT_PUBLIC_CONTENT_BASE_URL;
 
-export default async function sitemap({
-  id,
-}: {
-  id: number;
-}): Promise<MetadataRoute.Sitemap> {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const locale = await getLocale();
   const res = await fetch(
     `${contentBaseUrl}/api/stories/?locale=${locale}&draft=false&depth=0`
